3c537a0be1a9a4beffb0107400478b03f28e0141
Torgeir Lien <sloooth@gmail.com>
2012-10-09 14:26:10 -0700

    added index number to result class

--- hw2/src/api/Result.java
+++ hw2/src/api/Result.java
@@ -6,16 +6,20 @@ public class Result implements Serializable
     static final long serialVersionUID = 227L; // Was missing 
 	private Object taskReturnValue;
     private long taskRunTime;
+    private int taskIndex;
 
-    public Result( Object taskReturnValue, long taskRunTime )
+    public Result( Object taskReturnValue, long taskRunTime , int taskIndex)
     {
         assert taskReturnValue != null;
         assert taskRunTime >= 0;
         this.taskReturnValue = taskReturnValue;
         this.taskRunTime = taskRunTime;
+        this.taskIndex = taskIndex;
     }
 
     public Object getTaskReturnValue() { return taskReturnValue; }
 
     public long getTaskRunTime() { return taskRunTime; }
+    
+    public int getTaskIndex() {return taskIndex; }
 }
